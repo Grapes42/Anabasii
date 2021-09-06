19101d1d09848e07f3c9fa8eb897134d049b5260
Grapes42 <f.inggrapes@gmail.com>
2021-09-07 00:27:00 +0100

    Created simple script for uploading files, upload.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-	phpinfo();
-?>
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+
+	<body>
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+			<input type="file" name="file">
+			<button type="submit" name="submit">UPLOAD</button>
+		</form>
+	</body>
+</html>
