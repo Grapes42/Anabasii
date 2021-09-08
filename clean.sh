f3d57f1392a4c9abcf91fd4065c36b90add488aa
Grapes42 <f.inggrapes@gmail.com>
2021-09-08 07:14:22 +0100

    Created script that runs on boot and continuously removes the oldest files outside of the storage limit (100mb at the moment for testing)

---
+++ clean.sh
@@ -0,0 +1,10 @@
+cd uploads &&
+  find . -type f \( ! -iname ".*" \) -printf '%T@ %b :%p\0' |
+    sort -zrn |
+    gawk -v RS='\0' -v ORS='\0' '
+      BEGIN {max = 10e9/100}
+      {du += 512 * $2}
+      du > max {
+        sub("[^:]*:", ""); print
+      }' | xargs -r0 rm -f
+
---
+++ startup.sh
@@ -0,0 +1,2 @@
+cd /var/www/html
+watch -n 10 ./clean.sh
