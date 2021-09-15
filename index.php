85743b76564abfd8409f98920df37d19fc48f19e
Grapes42 <f.inggrapes@gmail.com>
2021-09-15 03:28:12 +0100

    Made the code much easier to read

--- index.php
+++ index.php
@@ -1,40 +1,64 @@
 <!DOCTYPE html>
 
 <html lang="en">
+
 	<head>
+
 		<meta charset="utf-8">
 		<title>Anabasii - Upload File</title>
 		<link rel="stylesheet" type="text/css" href="styles.css?version=51">
-	</head>
 
-	<header>
-	<h5>
-	<a href="index.php">Home</a> |
-	<a href="https://github.com/Grapes42/Anabasii">Github Repo</a>
-	</h5>
-	</header>
+	</head>
+	
 
 	<body>
-	<center>
-		<div class="titleclass">
-		<h1>Anabasii</h1>
-		<h2>Simple large file sharer</h2>
-		</div>
 
-		<div class="bodyclass">
-		<hr>
-		<h3>Select browse to choose a file<br>then upload to get a link to share</h3>
-		<h4>Mobile is not supported yet. Current size limit is 1gb, please upload folders as zips, tar.gz, or other compression files</h4>
-		<form action="upload.php" method="post" enctype="multipart/form-data">
-			<input type="file" name="file">
-			<button type="submit" name="submit">Upload</button>
-		</form>
-		<hr>
-		<h3>What is Anabasii?</h3>
-		<p><span title="What does Anabasii mean?"><a href="definition.html">Anabasii</a></span> is an opensource (<span title="Github Repository"><a href="https://github.com/Grapes42/Anabasii">Github</a><span>), free to use file sharer hosted on a little raspberry pi.
-		<br><br>
-		It allows you to share any videos or other files without the small limits most communication programs provide.</p>
-		</div>
-	</center>
+		<header>
+
+			<h5>
+			<a href="index.php">Home</a> |
+			<a href="https://github.com/Grapes42/Anabasii">Github Repo</a>
+			</h5>
+
+		</header>
+
+
+		<center>
+
+			<div class="titleclass">
+
+				<h1>Anabasii</h1>
+				<h2>Simple large file sharer</h2>
+
+			</div>
+
+
+			<div class="bodyclass">
+
+				<hr>
+
+				<h3>Select browse to choose a file<br>then upload to get a link to share</h3>
+				<h4>Mobile is not supported yet. Current size limit is 1gb, please upload folders as zips, tar.gz, or other compression files</h4>
+
+
+				<form action="upload.php" method="post" enctype="multipart/form-data">
+
+					<input type="file" name="file">
+					<button type="submit" name="submit">Upload</button>
+
+				</form>
+
+
+				<hr>
+
+				<h3>What is Anabasii?</h3>
+				<p><span title="What does Anabasii mean?"><a href="definition.html">Anabasii</a></span> is an opensource (<span title="Github Repository"><a href="https://github.com/Grapes42/Anabasii">Github</a><span>), free to use file sharer hosted on a little raspberry pi.</p>	
+				<p>It allows you to share any videos or other files without the small limits most communication programs provide.</p>
+
+			</div>
+
+		</center>
+
 	</body>
+
 </html>
--- upload.php
+++ upload.php
@@ -1,71 +1,88 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
+	
+	<head>
 
-<head>
-<title>Anabasii - Uploaded</title>
-<link rel="stylesheet" type="text/css" href="styles.css?version=51">
-</head>
+		<title>Anabasii - Uploaded</title>
+		<link rel="stylesheet" type="text/css" href="styles.css?version=51">
 
-<header>
-<h5>
-<a href="index.php">Home</a> |
-<a href="https://github.com/Grapes42/Anabasii">Github Repo</a>
-</h5>
-</header>
+	</head>
 
-<body>
-<center>
-<div class="titleclass">
-<h1>Anabasii</h1>
-<h2>Simple large file sharer</h2>
-</div>
-<h3>Copy this link and send it to your friends!</h3>
-<p>
-<?php
-if (isset($_POST['submit'])) {
-    $file = $_FILES['file'];
 
-    $fileName = $_FILES['file']['name'];
-    $fileTmpName = $_FILES['file']['tmp_name'];
-    $fileSize = $_FILES['file']['size'];
-    $fileError = $_FILES['file']['error'];
-    $fileType = $_FILES['file']['type'];
+	<body>
 
-    $fileExt = explode('.', $fileName);
-    $fileActualExt = strtolower(end($fileExt));
+		<header>
 
-    $allowed = array('jpg', 'jpeg', 'png', 'pdf', 'txt', 'mp4', 'gif');
+			<h5>
+			<a href="index.php">Home</a> |
+			<a href="https://github.com/Grapes42/Anabasii">Github Repo</a>
+			</h5>
 
-    //if(in_array($fileActualExt, $allowed))
-    //{
-        if($fileError === 0)
-        {
-            if($fileSize < 1000*1000*1000) // 1GB
-            {
-                $fileNameNew = uniqid('', true).".".$fileActualExt;
-                $fileDestination = 'uploads/'.$fileNameNew;
-                move_uploaded_file($fileTmpName, $fileDestination);
-                echo "http://anabasii.ddns.net/".$fileDestination;
-            }
-            else
-            {
-                echo "Your file is too large";
-            }
-        }
-        else
-        {
-            echo "There was an error uploading your file";
-        }
-    //}
-    //else
-    //{
-    //    echo "File type not allowed";
-    //}
-}
-?>
-</p>
-</center>
-</body>
+		</header>
+
+
+		<center>
+
+			<div class="titleclass">
+
+				<h1>Anabasii</h1>
+				<h2>Simple large file sharer</h2>
+
+			</div>
+
+
+			<div class="bodyclass">
+
+				<h3>Copy this link and send it to your friends!</h3>
+
+				<p>
+
+					<?php
+
+					if (isset($_POST['submit']))
+					{
+
+						$file = $_FILES['file'];
+
+						$fileName = $_FILES['file']['name'];
+						$fileTmpName = $_FILES['file']['tmp_name'];
+						$fileSize = $_FILES['file']['size'];
+						$fileError = $_FILES['file']['error'];
+						$fileType = $_FILES['file']['type'];
+
+						$fileExt = explode('.', $fileName);
+						$fileActualExt = strtolower(end($fileExt));
+
+						if($fileError === 0)
+						{
+							if($fileSize < 1000*1000*1000) // 1GB
+							{
+								$fileNameNew = uniqid('', true).".".$fileActualExt;
+								$fileDestination = 'uploads/'.$fileNameNew;
+								move_uploaded_file($fileTmpName, $fileDestination);
+								echo "http://anabasii.ddns.net/".$fileDestination;
+							}
+							else
+							{
+								echo "Your file is too large";
+							}
+						}
+						else
+						{
+						    echo "There was an error uploading your file";
+						}
+
+					}
+
+					?>
+
+				</p>
+
+			</div>
+
+		</center>
+
+	</body>
 
 </html>
