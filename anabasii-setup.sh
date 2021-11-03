cb657e4ecae64802d4e8d2f791c862478b582f3c
Grapes42 <f.inggrapes@gmail.com>
2021-11-03 14:27:10 +1300

    Created setup script

---
+++ anabasii-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+then
+	echo "Please run this script as root"
+	exit
+else
+	apt update && apt upgrade
+	apt install apache2 php7.3 php7.3-fpm
+	a2enmod proxy_fcgi setenvif
+	rm -rf /var/www/html
+	mv ~/Anabasii /var/www/html
+	chmod -R 777 /var/www/html/uploads
+	cp /var/www/html/setupFiles/000-default.conf /etc/apache2/sites-available/000-default.conf
+	cp /var/www/html/setupFiles/php.ini /etc/php/7.3/fpm/php.ini
+	cp /var/www/html/setupFiles/anabasii-startup.sh /etc/init.d/anabasii-startup.sh
+	sudo update-rc.d anabasii-startup.sh defaults
+	reboot
+fi
